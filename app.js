df09f9804e275a2f80696a689bc7c98dd8de041a
nitesh <rai.k.nitesh@gmail.com>
2021-10-01 14:49:03 +0530

    js

--- app.js
+++ app.js
@@ -2,4 +2,7 @@
 
 // alert("THIS SCRIPT WORKS!") : GETS POPUP IN YOUR BROWSER WITH GIVEN MESSAGE
 
-alert("THIS SCRIPT WORKS!" + username);
+
+console.log("script is working from a different file");
+// alert("THIS SCRIPT WORKS!")
+// alert("THIS SCRIPT WORKS!" + username);
