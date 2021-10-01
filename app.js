899e71f67b11ae4b8b2d6716b78c5675630f9a78
nitesh <rai.k.nitesh@gmail.com>
2021-10-01 17:00:00 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var btnTranslate= document.querySelector("#btn-translate");
 var txtInput=document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
+
+// outputDiv.innerText = "Nitesh Rai"
+console.log(outputDiv);
 
 function clickEventHandler(){
-    console.log("clicked!");
-    console.log("input", txtInput.value)
+    outputDiv.innerText = "ajsjsjsjsjsj" + txtInput.ariaValueMax;
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
 
 
-// ex-04
+
+
+
