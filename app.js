095928b91766ea2a6cc184a239feab7e44e3dc63
nitesh <rai.k.nitesh@gmail.com>
2021-10-01 14:45:55 +0530

    js code

---
+++ app.js
@@ -0,0 +1,5 @@
+// console.log("script is working from a different file"); : PRINT MESSAGE
+
+// alert("THIS SCRIPT WORKS!") : GETS POPUP IN YOUR BROWSER WITH GIVEN MESSAGE
+
+alert("THIS SCRIPT WORKS!" + username);
