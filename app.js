93c38f82e3e2010f429adf7a47f53fa42b364174
nitesh <rai.k.nitesh@gmail.com>
2021-10-04 19:09:21 +0530

    js code updated

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
-var btnTranslate= document.querySelector("#btn-translate");
-var txtInput=document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-// outputDiv.innerText = "Nitesh Rai"
-console.log(outputDiv);
 
-function clickEventHandler(){
-    outputDiv.innerText = "ajsjsjsjsjsj" + txtInput.ariaValueMax;
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
+}
+
+function errorHandler(error) {
+    console.log("error occured", error)
+    alert("something wrong with server! try again after some time")
+}
+
+
+
+function clickEventHandler() {
+    
+    var inputText = txtInput.value; //taking input
+
+    //calling server for processing
+    fetch(getTranslationURL(text))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText; //output
+        }
+          
+        .catch(errorHandler)
 }
 
 btnTranslate.addEventListener("click", clickEventHandler)
-
-
-
-
-
