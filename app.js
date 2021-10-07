ebaa314b3c1595091fbd06cc8806b11605e587ca
nitesh <rai.k.nitesh@gmail.com>
2021-10-08 00:23:05 +0530

    js update

--- app.js
+++ app.js
@@ -2,32 +2,31 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+// var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
 
 function getTranslationURL(input) {
     return serverURL + "?" + "text=" + input
 }
 
 function errorHandler(error) {
-    console.log("error occured", error)
+    console.log("error occured", error);
     alert("something wrong with server! try again after some time")
 }
 
 
+function clickHandler() {
+    var inputText = txtInput.value; // taking input
 
-function clickEventHandler() {
-    
-    var inputText = txtInput.value; //taking input
-
-    //calling server for processing
-    fetch(getTranslationURL(text))
+    // calling server for processing
+    fetch(getTranslationURL(inputText))
         .then(response => response.json())
         .then(json => {
             var translatedText = json.contents.translated;
-            outputDiv.innerText = translatedText; //output
-        })
-          
+            outputDiv.innerText = translatedText; // output
+           })
         .catch(errorHandler)
 };
 
